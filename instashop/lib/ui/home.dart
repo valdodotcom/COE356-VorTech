0cceab90f30b5886106c7e981a30e9b4f54033d5
Valdo Abruquah <valdoato@gmail.com>
2021-07-20 18:27:49 +0000

    Icon Added To Home Screens
    ListView added to Login page to make it scrollable after putting in the shopping cart icon

--- instashop/lib/ui/home.dart
+++ instashop/lib/ui/home.dart
@@ -13,6 +13,8 @@ class Home extends StatelessWidget {
             child: new Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+                  new Icon(Icons.shopping_cart_rounded,
+                  size: 250.0,),
                   new Text(
                     "instaShop",
                     style: new TextStyle(
@@ -55,51 +57,54 @@ class SignInPage extends StatelessWidget {
     var _emailController = TextEditingController();
     var _passwordController = TextEditingController();
     return new Scaffold(
-      // appBar: new AppBar(
-      //   backgroundColor: Color(0xff00eaff),
-      // ),
-      body: new Container(
+      backgroundColor:  Color(0xff00eaff),
+      body: new ListView(
         padding: EdgeInsets.all(40.0),
-        color: Color(0xff00eaff),
-        alignment: Alignment.center,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text("Welcome back!"),
-            new Text("Please sign in to continue"),
-            new TextField(
-              controller: _emailController,
-              decoration: new InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            new TextField(
-              controller: _passwordController,
-              decoration: new InputDecoration(
-                labelText: "Password",
-              ),
-              obscureText: true,
-            ),
-            new TextButton(
-                onPressed: () => debugPrint("Forgot password pressed"),
-                child: new Text("Forgot your password?")),
-            new ElevatedButton(
-                child: new Text("Sign in"),
-                onPressed: () {
-                  var router = new MaterialPageRoute(
-                      builder: (BuildContext context) => new ShopPage());
-                  Navigator.of(context).push(router);
-                }),
-            new TextButton(
-                onPressed: () {
-                  var router = new MaterialPageRoute(
-                      builder: (BuildContext context) => new SignInPage());
-                  Navigator.of(context).push(router);
-                },
-                child: new Text("Don't have an account? Sign up"),)
-          ],
+            new Column(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              //   mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Padding(padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0)),
+        new Icon(Icons.shopping_cart_rounded,
+        size: 140.0),
+      new Text("Welcome back!",
+        style: new TextStyle(fontSize: 30),),
+      new Text("Please sign in to continue"),
+      new TextField(
+        controller: _emailController,
+        decoration: new InputDecoration(
+          labelText: "Email",
         ),
       ),
+      new TextField(
+        controller: _passwordController,
+        decoration: new InputDecoration(
+          labelText: "Password",
+        ),
+        obscureText: true,
+      ),
+      new TextButton(
+          onPressed: () => debugPrint("Forgot password pressed"),
+          child: new Text("Forgot your password?")),
+      new ElevatedButton(
+          child: new Text("Sign in"),
+          onPressed: () {
+            var router = new MaterialPageRoute(
+                builder: (BuildContext context) => new ShopPage());
+            Navigator.of(context).push(router);
+          }),
+      new TextButton(
+        onPressed: () {
+          var router = new MaterialPageRoute(
+              builder: (BuildContext context) => new SignInPage());
+          Navigator.of(context).push(router);
+        },
+        child: new Text("Don't have an account? Sign up"),),
+        ]
+
+            )],
+        ),
     );
   }
 }
